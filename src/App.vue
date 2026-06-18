135f66b31567a293a1dd84cd560d35102a38bd8d
DerouillonAlice <pro@alicedr.com>
2026-06-18 09:06:38 +0200

    fix: replace flowers

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,9 @@ import pinkFlowerImg from '@/assets/img/pink_flower.svg'
   <!-- Zone footer : fleurs derrière la vague et le fond vert -->
   <div class="relative">
     <img :src="yellowFlowerImg" aria-hidden="true"
-      class="absolute -top-56 left-2 sm:left-6 w-64 sm:w-80 z-0 pointer-events-none select-none" />
+      class="absolute -top-75 left-2 sm:left-6 w-64 sm:w-80 z-0 pointer-events-none select-none" />
     <img :src="pinkFlowerImg" aria-hidden="true"
-      class="absolute -top-52 right-2 sm:right-6 w-56 sm:w-80 z-0 pointer-events-none select-none" />
+      class="absolute -top-78 right-2 sm:right-6 w-56 sm:w-80 z-0 pointer-events-none select-none" />
     <WaveEdge class="relative z-10" color="var(--color-primary)" position="top" :size="60" />
     <AppFooter class="relative z-10" />
   </div>
