2a03499becc39124bb155c9302983b26f4255682
DerouillonAlice <pro@alicedr.com>
2026-06-18 08:53:28 +0200

    fix(router): scroll to top on navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: () => ({ top: 0 }),
   routes: [
     { path: '/',        name: 'home',   component: HomeView },
     { path: '/regles',  name: 'regles', component: () => import('@/views/RulesView.vue') },
